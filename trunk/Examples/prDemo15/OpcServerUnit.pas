440b35d23f95e61aa455966c0c6a2c99d83d0ff8
engycz@gmail.com <engycz@gmail.com@af38fc98-952c-c9d5-e397-6ed6193bb087>
2012-07-19 07:23:07 +0000

    Changed copyright notice in files.

--- trunk/Examples/prDemo15/OpcServerUnit.pas
+++ trunk/Examples/prDemo15/OpcServerUnit.pas
@@ -1,8 +1,11 @@
 {------------------------------------------------------------}
 {                    prOpc Toolkit                           }
 { Copyright (c) 2000, 2001 Production Robots Engineering Ltd }
-{ mailto: prOpcKit@prel.co.uk                                }
-{ http://www.prel.co.uk                                      }
+{                                                            }
+{ mailto engycz@gmail.com                                    }
+{ http://code.google.com/p/propc/                            }
+{ original source mailto: prOpcKit@prel.co.uk                }
+{ original source http://www.prel.co.uk                      }
 {------------------------------------------------------------}
 unit OpcServerUnit;
 {this example implements a standard server without rtti. Instead of
